85f92588ece406aa93fbd9d69945318c1862024b
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-11-25 09:58:44 +0100

    re installed adn removed unnecessary files

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,56 +1,11 @@
-import Header from "./components/Header";
-import Todo from "./components/Todo";
-import Greeting from "./components/Greeting";
-// import Meme from "./components/Meme";
-// import ClassBased from "./components/ClassBased";
-// import State from "./components/State";
-// import Event from "./components/EventHandler";
-// import Count from './components/Count'
-import LifeCycle from "./learning/LifeCycleMethods";
-import Messages from "./learning/Messages";
-import Logged from "./learning/Logged";
-import Blah from "./learning/Blah";
-// import Form from "./learning/Form";
-// import FormII from "./learning/FormII";
-import MemeII from "./learning/Meme";
+import React from 'react'
 
-import React, { Component } from "react";
-
-export default class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      answer: "Yes",
-      isLoading: true,
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        isLoading: false,
-      });
-    }, 1500);
-  }
-  render() {
-    return (
-      <div>
-        <Header />
-        <Greeting />
-        {/* <h1>Is state important to know? {this.state.answer}</h1>
-        <ClassBased /> */}
-        <Todo />
-        {/* <Meme />
-        <State />
-        <Event />
-        <Count /> */}
-        <LifeCycle isLoading={this.state.isLoading} />
-        <Messages />
-        <Logged />
-        <Blah />
-        {/* <FormII /> */}
-        <MemeII />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      hello world
+    </div>
+  )
 }
+
+export default App
--- learn-react/src/components/ClassBased.js
+++
@@ -1,7 +0,0 @@
-import React, { Component } from "react";
-
-export default class ClassBased extends Component {
-  render() {
-    return <div>hello there</div>;
-  }
-}
