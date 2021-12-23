7d66fd6a1bebc2e2101bbb83918059524980b09d
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2021-12-23 10:33:33 +0100

    configured tailwind css

--- learn-react/src/App.js
+++ learn-react/src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import './App.css';
 
 function App() {
   return (
-    <div>
-      hello world
+    <div className="container p-4 mx-auto bg-gray-600 text-gray-200">
+      <h1 className='capitalize text-3xl font-bold font-mono text-center'>hello world</h1>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
