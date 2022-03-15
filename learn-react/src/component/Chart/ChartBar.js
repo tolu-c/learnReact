2a53302770dea55af16045087df84d24c73df119
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-03-15 07:01:29 +0000

    fixed bug

--- learn-react/src/component/Chart/ChartBar.js
+++ learn-react/src/component/Chart/ChartBar.js
@@ -1,7 +1,7 @@
 function ChartBar(props) {
   let barFillHeight = "0%";
 
-  if (props.max > 0) {
+  if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
   }
   console.log(barFillHeight);
