d631eef2e6cea88ff87342b1e36842288123de42
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-03-15 20:17:14 +0100

    added styles

--- learn-react/src/component/Chart/ChartBar.js
+++ learn-react/src/component/Chart/ChartBar.js
@@ -7,9 +7,9 @@ function ChartBar(props) {
   console.log(barFillHeight);
 
   return (
-    <div className='bg-purple-100 p-2 w-full text-center'>
-      <div className='h-36 flex justify-center'>
-        <div style={{ height: barFillHeight }} className="bg-purple-800 w-3 h-auto rounded-md border-purple-200 border-1 "></div>
+    <div className='bg-purple-100 p-2 w-full text-center flex justify-center flex-col items-center'>
+      <div className='h-36 flex justify-center items-center w-3 bg-purple-400 rounded-md'>
+        <div style={{ height: barFillHeight }} className="bg-purple-800 w-5/6 rounded-md border-purple-200 border-1 justify-items-center"></div>
       </div>
 
       <div className='text-purple-800 font-semibold text-lg'>{props.label}</div>
