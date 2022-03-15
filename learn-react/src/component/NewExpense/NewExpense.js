f9e1f1d8a799e8404b8ca4dfd72365961ba30a45
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-03-15 02:22:38 +0000

    assignment part 1

--- learn-react/src/component/NewExpense/NewExpense.js
+++ learn-react/src/component/NewExpense/NewExpense.js
@@ -1,6 +1,8 @@
 import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
 
 function NewExpense(props) {
+  const [isEditing, setIsEditing] = useState(false);
   const saveExpenseData = (enteredExpenseData) => {
     const expenseData = {
       ...enteredExpenseData,
@@ -10,10 +12,22 @@ function NewExpense(props) {
     // passing data to parent component => app
     props.onAddedExpense(expenseData);
   };
-  
+
+  function startEditing() {
+    setIsEditing(true);
+  }
+
   return (
     <div className="bg-blue-400 p-6">
-      <ExpenseForm onSaveExpenseData={saveExpenseData} />
+      {!isEditing && (
+        <button
+          onClick={startEditing}
+          className="w-3/4 text-2xl bg-blue-700 p-4 text-white font-bold capitalize rounded-2xl shadown-md shadow-blue-400 hover:shadow-2xl"
+        >
+          Add New Expense
+        </button>
+      )}
+      {isEditing && <ExpenseForm onSaveExpenseData={saveExpenseData} />}
     </div>
   );
 }
