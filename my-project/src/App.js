68ef33b8856c3bc42e942a51b129d82756f00a61
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-04 02:16:24 +0100

    planning component and styles component

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,7 +1,9 @@
+import AddUser from './components/users/AddUser'
+
 export default function App() {
   return (
-    <div className='p-2 bg-teal-100 border border-teal-600'>
-      <p className='text-lg font-bold text-teal-800'>hello world</p>
+    <div className="flex items-center justify-center my-10 bg-gray-100 container mx-auto p-3 border border-gray-400 shadow-md shadow-teal-600/20">
+      <AddUser />
     </div>
-  )
+  );
 }
---
+++ my-project/src/components/users/AddUser.js
@@ -0,0 +1,51 @@
+function AddUser(props) {
+  function addUser(event) {
+    event.preventDefault();
+  }
+
+  return (
+    <form
+      onSubmit={addUser}
+      className="flex flex-col gap-4 items-center justify-center w-5/6"
+    >
+      <div className="flex flex-col gap-2 w-full">
+        <label
+          htmlFor="username"
+          className="text-lg text-gray-800 font-medium capitalize"
+        >
+          username
+        </label>
+        <input
+          type="text"
+          id="username"
+          name="username"
+          className="text-base text-gray-600 focus:border-gray-600 focus:ring-gray-600 invalid:text-red-500 invalid:border-red-500 focus:invalid:border-red-500 focus:invalid:ring-red-500 invalid:bg-red-200"
+        />
+      </div>
+
+      <div className="flex flex-col gap-2 w-full">
+        <label
+          htmlFor="age"
+          className="text-lg text-gray-800 font-medium capitalize"
+        >
+          age (years)
+        </label>
+        <input
+          type="number"
+          id="age"
+          name="age"
+          className="text-base text-gray-600 focus:border-gray-600 focus:ring-gray-600 invalid:text-red-500 invalid:border-red-500 focus:invalid:border-red-500 focus:invalid:ring-red-500 invalid:bg-red-200"
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="w-full bg-black text-white capitalize py-3 text-lg font-medium flex items-center justify-center"
+      >
+        Add user
+      </button>
+    </form>
+  );
+}
+
+export default AddUser;
