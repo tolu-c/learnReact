4a88f41327406c158ae29b77fdb214a9f909e271
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-04 01:50:50 +0100

    created new project

---
+++ my-project/src/App.js
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className='p-2 bg-teal-100 border border-teal-600'>
+      <p className='text-lg font-bold text-teal-800'>hello world</p>
+    </div>
+  )
+}
