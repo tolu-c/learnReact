debe8ecc0d5745383bbcf75e6586c83642a43e06
Toluwanimi Adeyemo <68569546+tolu-c@users.noreply.github.com>
2022-04-05 08:53:48 +0100

    useEffect summary

--- new-project/src/components/Login/Login.js
+++ new-project/src/components/Login/Login.js
@@ -12,6 +12,10 @@ const Login = (props) => {
   const [formIsValid, setFormIsValid] = useState(false);
 
   useEffect(() => {
+    console.log('effect running')
+  }, [])
+
+  useEffect(() => {
     const identifier = setTimeout(() => {
       setFormIsValid(
         enteredEmail.includes("@") && enteredPassword.trim().length > 6
