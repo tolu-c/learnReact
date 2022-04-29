79b4a30dec7cb8ad313fa4c8354921943db69e8c
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-04-29 16:56:23 +0100

    meals component

--- order-my-food/src/App.js
+++ order-my-food/src/App.js
@@ -1,9 +1,13 @@
 import Header from "./components/layout/Header";
+import Meals from "./components/meals/Meals";
 
 export default function App() {
   return (
     <div>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </div>
   );
 }
