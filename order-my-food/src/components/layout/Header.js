7a62de9155596596f245f6b38cc986e1e0eec22a
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-04-29 11:43:23 +0100

    edited header component

--- order-my-food/src/components/layout/Header.js
+++ order-my-food/src/components/layout/Header.js
@@ -1,13 +1,13 @@
 import { Fragment } from "react";
-import { ShoppingCartIcon } from "@heroicons/react/solid";
 import meals from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 function Header(props) {
   return (
     <Fragment>
       <header className="w-full flex justify-between items-center text-white bg-orange-600 px-12 py-3">
         <h1 className='text-2xl font-bold font-sans'>MyMeals</h1>
-        <ShoppingCartIcon className="h-6 w-6" />
+        <HeaderCartButton />
       </header>
 
       <div className="w-full">
