9416678511ad5254766de0514be845064e399ccc
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-02 17:49:06 +0100

    installed dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from './components/UI/Button';
 
 import './App.css';
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false)
+
+  const toggleParagraph = () => {
+    setShowParagraph(prevShowParagraph => !prevShowParagraph)
+  }
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph && <p>This is a new paragraph!</p>}
+      <Button onClick={toggleParagraph}>show paragraph</Button>
     </div>
   );
 }
