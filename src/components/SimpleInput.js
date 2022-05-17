0501117514ae501a9edb024d60c4d6e9a9d58d23
Toluwanimi Adeyemo <tolu.adeyemo7@gmail.com>
2022-05-17 13:58:37 +0100

    form validity

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,11 +1,18 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const SimpleInput = (props) => {
   const [name, setName] = useState("");
   const [nameTouched, setNameTouched] = useState(false);
+  // const [formIsValid, setFormIsValid] = useState(false);
 
   const nameIsValid = name.trim() !== "";
   const nameIsInvalid = !nameIsValid && nameTouched;
+
+  let formIsValid = false;
+
+  if (nameIsValid) {
+    formIsValid = true;
+  }
 
   const nameChangeHandler = (event) => {
     event.preventDefault();
@@ -28,7 +35,7 @@ const SimpleInput = (props) => {
 
     console.log(name);
     setName("");
-    setNameTouched(false)
+    setNameTouched(false);
   };
 
   const nameInputClasses = nameIsInvalid
@@ -49,7 +56,7 @@ const SimpleInput = (props) => {
         {nameIsInvalid && <p className="error-text">Name must not be empty</p>}
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button disabled={!formIsValid}>Submit</button>
       </div>
     </form>
   );
